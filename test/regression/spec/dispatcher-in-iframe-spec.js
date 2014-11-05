7e89bdb1fff7f85b04a2d661f74c477d11b44fc4
Ed Eustace <ed.eustace@gmail.com>
2014-11-05 20:47:52 +0000

    set up receiverReady so that it'll work if the dispatcher is in an iframe

---
+++ test/regression/spec/dispatcher-in-iframe-spec.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+
+describe('dispatcher in the child page', function(){
+
+  var baseUrl = process.env.MSGR_BASE_URL || 'http://localhost:5000';
+
+  console.log('browser: ', browser);
+
+  it('dispatcher in the child page', function(done){
+    browser
+      .url(baseUrl + '/test/regression/samples/dispatcher-in-iframe/root.html')
+      .pause(400)
+      .frame("child")
+      .waitForText('#output', 1000)
+      .getText('#output', function(err, html) {
+        assert.equal('pong', html);
+      }).call(done);
+  });
+
+});
